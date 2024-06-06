7ba5104a374bb04ddb85cfab9a27f69960c55518
Fábio Correia Lima <118375746+FabioCorreiaLima@users.noreply.github.com>
2024-06-05 21:02:00 -0300

    Delete app_ge_eventos directory

--- app_ge_eventos/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import Evento
-
-class EventoForm(forms.ModelForm):
-    class Meta:
-        model = Evento
-        fields = ['titulo', 'descricao', 'data_inicio', 'data_termino', 'local']
